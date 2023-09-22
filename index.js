ad5d8061f1a60562dd4d893a346e6e86bee50def
Jeiojey <signofaries@gmail.com>
2023-09-22 15:56:15 +0300

    feat: cart function in index page (JS test)

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const featuredItems = document.querySelector('.featured__items');
 const cartNum = document.querySelector('.basket_num');
-const cartItemsBox = document.querySelector('.cart__items');
-console.log(cartItemsBox);
+const cart = document.querySelector('.cart__contatiner');
 
-async function fetchData(container) {
+async function fetchData(container, cart) {
     try {
         const response = await fetch("./data.json");
         console.log(response);
@@ -14,10 +13,10 @@ async function fetchData(container) {
         const data = await response.json();
         // console.log(data);
 
-        data.forEach(({ name, image, description, price }) => {
+        data.forEach(({ id, name, image, description, price }) => {
             const featuredItem = `
 
-    <div class="featured__item">
+    <div class="featured__item" >
 
         <img src="${image}" alt="Product image" class="featured__img">
         <a href="product.html" class="featured__item-link">
@@ -27,7 +26,7 @@ async function fetchData(container) {
                 <p class="featured__item-price">$${price}</p>
             </div>
         </a>
-        <div class="featured__add">
+        <div class="featured__add" data-id="${id}">
         <svg width="27" height="25" viewBox="0 0 27 25" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -49,19 +48,52 @@ async function fetchData(container) {
         featuredItems.addEventListener('click', (e) => {
             if (e.target.classList.contains('featured__add')) {
                 cartNum.innerHTML++;
-                // addtoCart();
-
             }
-            console.log(e.target.classList);
+            for (let i = 0; i < data.length; i++) {
+                const element = data[i];
+                if (e.target.dataset.id == data[i].id) {
+                    const cartItem = `
+                    <div class="cart__item">
+                        <img src="${data[i].cart_image}" alt="product image">
+                        <div class="cart__item_content">
+                            <h2 class="cart__heading">${data[i].name}</h2>
+                            <p class="cart__text">Price: <span class="cart__text_price">$${data[i].price}</span></p>
+                            <p class="cart__text">Color: Red</p>
+                            <p class="cart__text">Size: Xl</p>
+                            <div class="cart__quantity">
+                                <label for="quantity1" class="cart__label">
+                                    <span class="cart__text">Quantity:</span>
+                                </label>
+                                <input type="number" name="" id="quantity1" min="0" value="${data[i].quantity}" class="cart__input">
+                            </div>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                            class="cart__close">
+                            <path
+                                d="M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z"
+                                fill="#575757" />
+                        </svg>
+                    </div>
+                    `;
+                    cart.insertAdjacentHTML("beforeend", cartItem);
+                }
+            }
+            const delBtns = document.querySelectorAll(".cart__close");
+            delBtns.forEach((el) => {
+                el.addEventListener("click", () => {
+                    const product = el.closest(".cart__item");
+                    product.remove();
+                });
+            });
+
         })
-
-
-
-
     }
     catch (error) {
         console.error(error);
     }
 }
 
-fetchData(featuredItems);
+
+
+
+fetchData(featuredItems, cart);
